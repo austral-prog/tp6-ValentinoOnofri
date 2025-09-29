ac1ee335f2554957a95246e613550da9529da1eb
ValentinoOnofri <vonofri@mail.austral.edu.ar>
2025-09-29 11:50:13 -0300

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,14 +1,17 @@
 # Replace the "ANSWER HERE" with your answer
 def remove_elements(list_to_remove_elements):
-    if len(list_to_remove_elements) > 6:
-        del list_to_remove_elements[5] #Ver
+    if len(list_to_remove_elements) >= 6:
+        del list_to_remove_elements[5]
+    if len (list_to_remove_elements) >= 5:
+        del(list_to_remove_elements[4])
+    if len (list_to_remove_elements) >= 1:
+        del(list_to_remove_elements[0])
+    return list_to_remove_elements
 
 def add_elements(list_to_add_elements): #No se pueden asignar a variables las funciones usadas. Uso un ejemplo por el error --> No va
-    #Uso lo de Lautaro --> No va
-    sample_input = ['Red', 'Green', 'White','Black']
-    sample_input.insert(0, 'Pink') #Primero al principio, despues al final
-    sample_input.append('Yellow')
-    return sample_input
+    list_to_add_elements.insert(0, "Pink")
+    list_to_add_elements.append("Yellow")
+    return list_to_add_elements
 
 def is_empty(list_to_check):
     if len(list_to_check) == 0:
@@ -30,6 +33,6 @@ def check_lists(list_to_compare1, list_to_compare2):
 def list_of_lists(list_of_lists_to_modify):
     slice1 = list_of_lists_to_modify[0][:2]
     slice2 = list_of_lists_to_modify[1][1:4]
-    slice3 = list_of_lists_to_modify[2][-2:] #Error aca
+    slice3 = list_of_lists_to_modify[2][-2:] #Error aca - Va! Usar ":", "en adelante" con slicing desde indices negativos
     lista_concatenada = [slice1, slice2, slice3]
     return lista_concatenada  # Remove this line and implement
